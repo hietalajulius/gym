476bda367ca5ba628d3e6a642e16f5626df8f6bf
Julius Hietala <julius.hietala@gmail.com>
2020-11-12 15:30:11 +0200

    Less prints

--- gym/envs/robotics/cloth/cloth.py
+++ gym/envs/robotics/cloth/cloth.py
@@ -7,7 +7,7 @@ MODEL_XML_PATH = os.path.join('cloth', 'cloth.xml')
 
 class ClothEnv(cloth_env.ClothEnv, utils.EzPickle):
     def __init__(self, **kwargs):
-        print("Env kwargs", kwargs)
+        print("Creating env")
         cloth_env.ClothEnv.__init__(
             self, MODEL_XML_PATH, **kwargs)
         utils.EzPickle.__init__(self)
